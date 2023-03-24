b3bc0fa9c050254cd98b7adbf553af44fd69ede0
iksagor0 <iksagor0@gmail.com@gmail.com>
2023-03-24 15:20:09 +0600

    Nav with responsive

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer>
+    <footer className="bg-slate-900 text-gray-400">
       <div className="container">Footer</div>
     </footer>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,94 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+import { AiOutlineCloseCircle } from "react-icons/ai";
+import { BsCaretDown } from "react-icons/bs";
+import { FaHamburger } from "react-icons/fa";
 
 export default function Header() {
+  const [showCategory, setShowCategory] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
+  const showSubMenu = () => setShowCategory(true);
+  const hideSubMenu = () => setShowCategory(false);
+
   return (
-    <header id="header" className="bg-slate-900">
-      <div className="container">
+    <header id="header" className="bg-slate-900 text-gray-300 py-2">
+      <div className="container flex items-center justify-between sm:justify-start gap-10">
         <div className="logo_area">
           <Link href="/">
             <Image src="/logo.png" alt="logo" width={80} height={53} />
           </Link>
+        </div>
+
+        <nav
+          id="navbar"
+          className="nav_btn_container sm:flex sm:ml-auto fixed top-0 right-0 sm:static duration-300"
+          style={{ right: showMenu ? 0 : -260 }}
+        >
+          <ul className="nav_item_wrapper sm:flex sm:gap-6 text-lg bg-neutral-900 sm:bg-slate-900 h-[100vh] sm:h-auto w-[200px] sm:w-auto px-12 sm:px-0 pt-14 sm:p-0 z-40">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li
+              className="relative"
+              onMouseOver={showSubMenu}
+              onMouseLeave={hideSubMenu}
+            >
+              <span
+                className="flex items-center gap-1"
+                onClick={() => setShowCategory(!showCategory)}
+              >
+                Category <BsCaretDown className="text-base pt-1" />
+              </span>
+
+              <ul
+                className="sub-menu min-w-[130px] absolute top-10 -left-4 bg-neutral-800 sm:bg-slate-900 px-4 pb-2 text-gray-400 text-sm"
+                style={{
+                  height: showCategory ? "auto" : 0,
+                  visibility: showCategory ? "visible" : "hidden",
+                }}
+              >
+                <li>
+                  <a href="">Smartphone</a>
+                </li>
+                <li>
+                  <a href="">PC</a>
+                </li>
+                <li>
+                  <a href="">Gaming</a>
+                </li>
+                <li>
+                  <a href="">Tips and Tricks</a>
+                </li>
+                <li>
+                  <a href="">Programming</a>
+                </li>
+                <li>
+                  <a href="">Science & Tech</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+            <li>
+              <Link href="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="flex gap-5">
+          <div className="btn_container">
+            <Link href={"/login"} className="btn-primary">
+              Login
+            </Link>
+          </div>
+
+          <div
+            className="hamburger-close sm:hidden z-50 text-2xl"
+            onClick={() => setShowMenu(!showMenu)}
+          >
+            {showMenu ? <AiOutlineCloseCircle /> : <FaHamburger />}
+          </div>
         </div>
       </div>
     </header>
